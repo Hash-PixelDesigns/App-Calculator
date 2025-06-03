cbef638109f83fcfca52c4fa5f3d967cadb0f92d
Harshana Patel <hash.pixeldesigns@gmail.com>
2025-06-03 11:46:02 +0200

    Lesson 4- AC Event Handlers

--- script.js
+++ script.js
@@ -20,7 +20,15 @@ function appendToDisplay(value) {
     display.value = value;
     } else if (currentValue === '0' && value === '.') {
         display.value = currentValue + value;
-    } else {
+    } else if (value ==='.') {
+        //Get the last number in the display
+        let lastNumber = currentValue.split ('/[+\-*/]').pop();
+        //Only add the decimal if the current number doesnt have one
+        if (!lastNumber.includes('.')){
+            display.value = currentValue + value;
+        }
+    
+    else {
         display.value = currentValue + value;
     }
 
@@ -28,13 +36,19 @@ function appendToDisplay(value) {
     justCalculated = false;
 
     console.log ('Display updated to: ', display.value);
+    }
 }
-
 
 function clearDisplay() {
     console.log('Clear buttomn pressed.');
 
-    alert('Clear Button was clicked');
+    display.value = '0';
+    justCalculated = false;
+
+    display.style.backgroundColor= #f0f0f0;
+    setTimeout (()=> {
+        display.style.backgroundColor = '';
+    },150);
 }
 
 function deleteLast () {
@@ -49,14 +63,40 @@ function deleteLast () {
         display.value =  currentValue.slice(0, -1);
     }
 
-    alert('Backspace button was clicked');
 }
 
 function calcutate () {
     console.log('Equals button pressed');
 
-    alert('Equals button was clicked');
+
 }
+
+document.addEventListener{'keydown', function (event) {
+    console.log ('Key pressed',event.key);
+
+    if (event.key >='0' && event.key <= '9') {
+        appendToDisplay(event.key);
+    } else if (event.key === '.') {
+        appendToDisplay('.');
+    } else if (event.key === '+') {
+        appendToDisplay('+');
+    } else if (event.key === '-') {
+        appendToDisplay('-');
+    } else if (event.key === '*') {
+        appendToDisplay('*');
+    } else if (event.key('/')) {
+        event.preventDefault();
+        appendToDisplay('/');
+    }
+    else if (event.key === 'Enter' || event,key === '=') {
+        calcutate();
+    }else if (event.key === 'Escape'|| event.key === 'c'|| event.key === 'C'){
+        clearDisplay();
+    }else if (event.key === 'Backspace') {
+        deleteLast();
+    }
+
+}}
 
 document. addEventListener('DOMContentLoaded', function(){
     console.log('calculater loaded succesfully');
